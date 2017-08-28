5ebccffa089b82c6bbf330a2ee334ac362ed6eb9
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-08-28 18:37:16 -0400

    added timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
-$(".container").html("hi everybody");
+var delayButtonAlert;
+
+
+
+// start button to begin game
+// create variables for results: correct / incorrect / unanswered
+// create object with questions, 4 possible answers, correct (a1, a2, etc)
+// do for loop to display Q&A on screen
+// have answers as radio buttons beneath question
+// click done to submit answers
+
+
+$(document).ready(function() {
+
+
+// set timer for 120 seconds
+setTimeout(timeUp, 1000 * 5);
+
+// out of time, end the game & calculate results
+function timeUp() {
+    console.log("time is up");
+}
+
+});
